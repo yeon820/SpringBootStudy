bc7d2ed864aa19c8dc9b71aece3a271eb868e94e
김가연 <150112222+yeon820@users.noreply.github.com>
2024-11-28 20:05:16 +0900

    입력한 정보 컨트롤러로 전달하기

--- member/src/main/java/com/example/member/controller/MemberController.java
+++ member/src/main/java/com/example/member/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.example.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
@@ -9,5 +11,15 @@ public class MemberController {
     @GetMapping("/member/save")
     public String saveForm() {
         return "save";
+    }
+
+    @PostMapping("/member/save")
+//    RequestParam으로 가져온 값을 변수 이름(memberEmail)에 담는다
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName) {
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
     }
 }
